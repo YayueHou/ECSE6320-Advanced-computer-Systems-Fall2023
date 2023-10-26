deccbd691c33288fe2fd97004fd3aa85e1b7a6b7
Yayue Hou <houy4@rpi.edu>
2023-10-26 10:22:56 -0400

    add plot

---
+++ Project 3/plot.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+result_file="Project 3/results/table1.csv"
+result_data=pd.read_csv(result_file)
+result_df=pd.DataFrame(result_data)
+
+p1=sns.relplot( kind="line",data=result_df,x="Latency avg (usec)",y="IOPS(k)",hue="Block Size(KiB)")
+p2=sns.relplot( kind="line",data=result_df,x="Latency avg (usec)",y="IOPS(k)",hue="Percentage of Write(%)")
+p3=sns.relplot( kind="line",data=result_df,x="Latency avg (usec)",y="IOPS(k)",hue="IO Depth")
+plt.show()
+print(result_df)
